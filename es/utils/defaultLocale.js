6d17347c048c5ea61a3c3aa5017f4987f19d9f26
Vitaly <vitalyb@gmail.com>
2019-12-26 00:25:39 +0200

    adding es build folder to git

---
+++ es/utils/defaultLocale.js
@@ -0,0 +1,9 @@
+module.exports = {
+  blank: 'Select a date...',
+  headerFormat: 'ddd, MMM Do',
+  todayLabel: {
+    long: 'Today'
+  },
+  weekdays: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+  weekStartsOn: 0
+};
---
+++ es/utils/defaultTheme.js
@@ -0,0 +1,16 @@
+module.exports = {
+  accentColor: '#448AFF',
+  floatingNav: {
+    background: '#FFFFFF',
+    chevron: '#65a3bb',
+    color: '#65a3bb'
+  },
+  headerColor: '#448AFF',
+  selectionColor: '#d1e0ff',
+  textColor: {
+    active: '#01222f',
+    default: '#333'
+  },
+  todayColor: '#458aff',
+  weekdayColor: '#FFFFFF'
+};
